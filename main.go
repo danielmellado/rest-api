a53664ff9a431199d3b65fcaff6294f55bfacef0
Daniel Mellado <dmellado@redhat.com>
2019-02-28 17:29:30 +0100

    Add process handler
    This commit adds process handler and modifies the node struct so it has
    a slice of processes as a var.

--- main.go
+++ main.go
@@ -26,23 +26,25 @@ import (
 )
 
 const (
-	CONN_PORT = ":8000"
+	// ConnPort defines the port to use for the api server
+	ConnPort = ":8000"
 )
 
-//Defines structs
+// Node struct representing the node object
 type Node struct {
-	Name      string   `json:"name`
-	TimeSlice float32  `json:"timeslice`
-	Cpu       float32  `json:"cpu"`
-	Mem       float32  `json:"mem"`
-	Process   *Process `json:"process,omitempty"`
+	Name      string     `json:"name`
+	TimeSlice float32    `json:"timeslice`
+	Cpu       float32    `json:"cpu"`
+	Mem       float32    `json:"mem"`
+	Process   []*Process `json:"process,omitempty"`
 }
 
+// Process struct representing the processes on the node
 type Process struct {
 	Name      string  `json:"name`
 	TimeSlice float32 `json:"timeslice"`
-	CpuUsed   float32 `json:"cpuused"`
-	MemUsed   float32 `json:"memused"`
+	CpuUsed   float32 `json:"cpu_used"`
+	MemUsed   float32 `json:"mem_used"`
 }
 
 var nodes []Node
@@ -79,16 +81,23 @@ func createProcessHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	for idx, node := range nodes {
 		if node.Name == params["nodename"] {
-			nodes[idx].Process = &process
+			nodes[idx].Process = append(nodes[idx].Process, &process)
 		}
 	}
 	json.NewEncoder(w).Encode(nodes)
 }
 
+func getProcessesHandler(w http.ResponseWriter, r *http.Request) {
+	for _, node := range nodes {
+		json.NewEncoder(w).Encode(node.Process)
+	}
+}
+
+// AddRoutes creates the mux routes for the rest api
 func AddRoutes(r *mux.Router) {
 	r.HandleFunc("/nodes/", nodesHandler).Methods("GET")
 	r.HandleFunc("/analytics/nodes/average/", todoHandler).Methods("GET")
-	r.HandleFunc("/analytics/processes/", todoHandler).Methods("GET")
+	r.HandleFunc("/analytics/processes/", getProcessesHandler).Methods("GET")
 	r.HandleFunc("/analytics/processes/{processname}/", todoHandler).Methods("GET")
 	r.HandleFunc("/metrics/node/{nodename}/", createNodeHandler).Methods("POST")
 	r.HandleFunc("/metrics/nodes/{nodename}/process/{processname}/", createProcessHandler).Methods("POST")
@@ -105,8 +114,9 @@ func main() {
 		return nil
 	})
 	// Initial dummy data
-	nodes = append(nodes, Node{Name: "kuryr", TimeSlice: 1.0, Cpu: 2.0, Mem: 3.0})
+	nodes = append(nodes, Node{Name: "node1", TimeSlice: 60.0, Cpu: 60.0, Mem: 40.0})
+	nodes = append(nodes, Node{Name: "node2", TimeSlice: 60.0, Cpu: 40.0, Mem: 60.0})
 	// Running server!
 	fmt.Println("Running server!")
-	log.Fatal(http.ListenAndServe(CONN_PORT, router))
+	log.Fatal(http.ListenAndServe(ConnPort, router))
 }
